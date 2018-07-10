be294b38d946bdc6383486029862aa51082575e1
Aditya Singla <adityasingla87@gmail.com>
2018-07-10 21:31:13 +0530

    Modal element creation

---
+++ modal.js
@@ -0,0 +1,49 @@
+$('document').ready(function() {
+$('#bt').click(function(){
+$('#outer_fixer').removeClass('ini')
+$('#inner_fixer').removeClass('ini')
+$('#heading').removeClass('ini')
+$('#content').removeClass('ini')
+$('#butt').removeClass('ini')
+$('#outer_fixer').addClass('outer_fixer_btn_click')
+$('#inner_fixer').addClass('inner_fixer_btn_click')
+$('#heading').addClass('heading')
+$('#content').addClass('content')
+$('#butt').addClass('butt')
+$('#outer_fixer').click(function(){
+
+$('#outer_fixer').addClass('ini');
+
+
+})
+
+$('#cross').click(function(){
+$('#outer_fixer').addClass('ini');
+
+})
+
+$('#bt1').click(function(){
+
+	$('#outer_fixer').addClass('ini');
+})
+
+$('#inner_fixer').click(function(){
+	return false
+})
+
+
+}
+
+
+	)
+
+
+
+
+
+
+
+
+
+}
+)
